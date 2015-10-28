4e2961bb65e2bf5da112b4726b86d0b74a0de4b9
woshibala <yang.song@colorado.edu>
2015-10-27 20:04:53 -0600

    Done

--- a.py
+++ a.py
@@ -1,65 +1,57 @@
+
 import sys
 X = None
 Y = None
 price = None
 matrix = None
-visited = None
-
+p = None
+n = None
 def input():
-	global X,Y,price,matrix,visited
+	global X,Y,p,n
 	for i in range(0,len(sys.argv)):
 		print i,": ",sys.argv[i]
 	file = open(sys.argv[1])
 	line = file.readline().split()
 	X = int(line[0])
 	Y = int(line[1])
-	print X,Y
 	line = file.readline().split()
 	n = int(line[0])
 	print n
-	p = []
+	p = [] 
 	line = file.readline().split()
 	while line != [] :
 		entry = (int(line[0]),int(line[1]),int(line[2]))#tuple[0]=ai;tuple[1]=bi;tuple[2]=ci
 		p.append(entry)
 		line = file.readline().split()
-	
+	file.close()
+
+def dp():
+	global p,n
 	price = []
 	for i in range(X+1):
 		price.append([])
 		for j in range(Y+1):
-			price[i].append(None)
-	for i in range(1,X+1):
-		for j in range(1,Y+1):
-			price[i][j] = -1
-	#store price for all size of products
-	for i in range(1,n):
+			price[i].append(0)
+	for i in range(n):
 		if price[p[i][0]][p[i][1]] < p[i][2]:
 			price[p[i][0]][p[i][1]] = p[i][2]
+	for i in price:
+		print i
 
-	#store the max price for a i*j cloth
 	maxprice = []
 	for i in range(X+1):
 		maxprice.append([])
 		for j in range(Y+1):
-			maxprice[i].append(None)
+			maxprice[i].append(0)
+	
+	matrix = []
+	for i in range(X+1):
+		matrix.append([])
+		for j in range(Y+1):
+			matrix[i].append(None)
+
 	for i in range(1,X+1):
 		for j in range(1,Y+1):
-			maxprice[i][j] = -1
-	maxprice[0][0] = 0
-	#maxprice[0][0] = 0		
-	matrix = []
-	for i in range(X):
-		matrix.append([])
-		for j in range(Y):
-			matrix[i].append(None)																																																																																																																																																																																																																																																																																																														z
-	sum = 0
-	h = 0
-	v = 0
-	#when the size of the cloth grows
-	previous = [None,None,None]
-	for i in range(X):
-		for j in range(Y):
 			max1 = 0
 			max2 = 0
 			a = 0
@@ -68,55 +60,38 @@ def input():
 			l1 = []
 			for x in range(1,i):
 				aaa = maxprice[x][j]+maxprice[i-x][j]
-				#if maxprice[x][j]*maxprice[i-x][j] != 0:
 				l1.append(aaa)
-				
 			if not len(l1) == 0:
 				max1 = max(l1)
 				a = l1.index(max1)+1
+			
 			#cut vertically
-		
 			l2 = []
 			for y in range(1,j):
 				bbb = maxprice[i][j-y]+maxprice[i][y]
-				#if maxprice[i][j-y]*maxprice[i][y] != 0:
-				l2.append(bbb)
-				
+				l2.append(bbb)				
 			if not len(l2) == 0:
 				max2 = max(l2)
 				b = l2.index(max2)+1
 
 			l3 = [max1,max2,price[i][j]]
 			maxprice[i][j] = max(l3)
+			print i,j,maxprice[i][j]
+			#store maxprice
+
 			if l3.index(max(l3)) == 0 :
-				#print "h"
-				sum += 1
-				h +=1
 				matrix[i][j] = [[a,j],[i-a,j],"v",a]
-	
 			elif l3.index(max(l3)) == 1:
-				#print "v"
-				sum += 1
-				v += 1
 				matrix[i][j] = [[i,j-b],[i,b],"h",b]
-				
-					#print "not cut"
-	
 
-	visited = []
-	for i in range(X):
-		visited.append([])
-		for j in range(Y):
-			visited[i].append(False)
-	#for i in visited:
-	#	print i
+ 	print maxprice[X][Y]
 
-	def DFS(i,j):
+ 	def DFS(i,j):
 		if matrix[i][j] == None:
-			return
+			return 0
 		else:
 			visited[i][j] = True
-			print i+1,j+1,matrix[i][j][2],matrix[i][j][3]
+			print i,j,matrix[i][j][2],matrix[i][j][3]
 			k = matrix[i][j][0]
 			x = k[0]
 			y = k[1]
@@ -128,17 +103,13 @@ def input():
 			if not visited[x][y]:
 				DFS(x,y)
 
-
-	print "maximum price is:",maxprice[X-1][Y-1]
+ 	visited = []
+	for i in range(X+1):
+		visited.append([])
+		for j in range(Y+1):
+			visited[i].append(False)
+	DFS(X,Y)
 	
-	
-	DFS(X-1,Y-1)
 
-
-
-
-def run():
-	input()
-
-run()
-
+input()
+dp()
--- dp1.py
+++ dp1.py
@@ -2,18 +2,15 @@ import sys
 X = None
 Y = None
 price = None
-h = None
-v = None
-tot = None
+matrix = None
 def input():
-	global X,Y,price,h,v,tot
+	global X,Y,price,matrix
 	for i in range(0,len(sys.argv)):
 		print i,": ",sys.argv[i]
-	file = open("cloth.txt")
+	file = open(sys.argv[1])
 	line = file.readline().split()
 	X = int(line[0])
 	Y = int(line[1])
-	print X,Y
 	line = file.readline().split()
 	n = int(line[0])
 	print n
@@ -23,85 +20,147 @@ def input():
 		entry = (int(line[0]),int(line[1]),int(line[2]))#tuple[0]=ai;tuple[1]=bi;tuple[2]=ci
 		p.append(entry)
 		line = file.readline().split()
-	
+	file.close()
 	price = []
-	for i in range(X):
+	print p
+	for i in range(X+1):
 		price.append([])
-		for j in range(Y):
-			price[i].append(-1)
+		for j in range(Y+1):
+			price[i].append(None)
+	for i in range(X+1):
+		for j in range(Y+1):
+			price[i][j] = -1
 	#store price for all size of products
 	for i in range(n):
 		if price[p[i][0]][p[i][1]] < p[i][2]:
 			price[p[i][0]][p[i][1]] = p[i][2]
 
 	#store the max price for a i*j cloth
-
 	maxprice = []
-	for i in range(0,X):
+	for i in range(X+1):
 		maxprice.append([])
-		for j in range(0,Y):
+		for j in range(Y+1):
 			maxprice[i].append(None)
-	for i in range(1,X):
-		for j in range(1,Y):
+	for i in range(1,X+1):
+		for j in range(1,Y+1):
 			maxprice[i][j] = -1
-	
-	tot= 0
+	maxprice[0][0] = 0
+	#maxprice[0][0] = 0																																																																																																																																																																																																																																																																																																																			z
+	sum = 0
 	h = 0
 	v = 0
-	maxprice[0][0] = 0	
+	matrix = []
+	for i in range(X+1):
+		matrix.append([])
+		for j in range(Y+1):
+			matrix[i].append(None)		
+	
 	def maxre(i,j):
-		global tot,h,v
 		if maxprice[i][j] != -1:
 			return maxprice[i][j]
 		else:
 			max1 = 0
 			max2 = 0
-			x = -1
-			y = -1
+			a = 0
+			b = 0
 			#cut horizontally
 			if j > 1:
 				l1 = []
-				for x in range(1,i):
-					aaa = maxre(x,j)+maxre(i-x,j)# maxprice[x][j]+maxprice[i-x][j]
+				for x in range(1,i-1):
+					if maxprice[x][j] == -1 and maxprice[i-x][j] == -1:
+						aaa = maxre(x,j)+maxre(i-x,j)# maxprice[x][j]+maxprice[i-x][j]
+					elif maxprice[x][j] != -1 and maxprice[i-x][j] == -1:
+						aaa = maxprice[x][j]+maxre(i-x,j)
+					elif maxprice[x][j] != -1 and maxprice[i-x][j] != -1:
+						aaa = maxprice[x][j] + maxprice[i-x][j]
+					else:
+						aaa = maxre(x,j)+maxprice[i-x][j]
 					l1.append(aaa)
-					
+				
 				if not len(l1) == 0:
 					max1 = max(l1)
-					x = l1.index(max1)
+					a = l1.index(max1)
 			#cut vertically
 			if i > 1:
 				l2 = []
-				for y in range(1,j):
-					bbb = maxre(i,j-y)+maxre(i,y)#maxprice[i][j-y]+maxprice[i][y]
+				for y in range(1,j-1):
+					if maxprice[i][j-y] == -1 and maxprice[i][y] == -1:
+						bbb = maxre(i,j-y)+maxre(i,y)#maxprice[i][j-y]+maxprice[i][y]
+					elif maxprice[i][j-y] != -1 and maxprice[i][y] == -1:
+						bbb = maxprice[i][j-y]+maxre(i,y)
+					elif maxprice[i][j-y] != -1 and maxprice[i][y] != -1:
+						bbb = maxprice[i][j-y]+maxprice[i][y]
+					else:
+						bbb = maxre(i,j-y)+maxprice[i][y]
 					l2.append(bbb)
 				
 				if not len(l2) == 0:
 					max2 = max(l2)
-					y = l2.index(max2)
-
+					b = l2.index(max2)
+		
+ 
 
 			l3 = [max1,max2,price[i][j]]
+			if max1 == price[i][j] or max2 == price[i][j]:
+				l3.pop()
 			maxprice[i][j] = max(l3)
-			if x*y == 0:
-				print "not cut"
-				return maxprice[i][j]
-			if l3.index(max(l3)) == 0:
-				print "h",i,j,y
-				h += 1
-				tot += 1
+			print i,j,l3.index(max(l3))
+			if l3.index(max(l3)) == 0 :
+				if  a != 0:
+					matrix[i][j] = [[a,j],[i-a,j],"v",a]
+					print matrix[i][j]
 			elif l3.index(max(l3)) == 1:
-				print "v",i,j,x
-				v += 1
-				tot += 1
-			else:
-				print "not cut"
+				if b != 0:
+					matrix[i][j] = [[i,j-b],[i,b],"h",b]
+					print matrix[i][j]
 			return maxprice[i][j]
+		return maxprice[X][Y]
+	def DFS(i,j):
+		global matrix
+		if matrix[i][j] == None or matrix[i][j] == -1:
+			return 
+		else:
+			visited[i][j] = True
+			print i,j,matrix[i][j][2],matrix[i][j][3]
+			line = [str(i),str(j),matrix[i][j][2],str(matrix[i][j][3]),"\n"]
+			file.writelines(line)
+			k = matrix[i][j][0]#child 1
+			x = k[0]
+			y = k[1]
+			if not visited[x][y]:
+				DFS(x,y)
+			k = matrix[i][j][1]#child 2
+			x = k[0]
+			y = k[1]
+			if not visited[x][y]:
+				DFS(x,y)
+	print maxre(X,Y)
+	
 
-	print maxre(X-1,Y-1)
-	print h,v,tot
+
+	visited = []
+	for i in range(X+1):
+		visited.append([])
+		for j in range(Y+1):
+			visited[i].append(False)
+	file = open("output.txt","w")
+	DFS(X,Y)
+	file.close()
+	
 
 
+	for i in maxprice:
+		print i
+	for i in price:
+		print i
+	for i in matrix:
+		print i
+
+	
+	
+	
 def run():
 	input()
+	print("Done")
 run()
 
